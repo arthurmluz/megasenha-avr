90a361879bd09ccaa6cb08b7d720445b504fb96b
Lucas Antunes <lucasba8@gmail.com>
2021-11-10 21:52:10 -0300

    refact: improve naming in keypad.c

--- keypad.c
+++ keypad.c
@@ -9,26 +9,26 @@ char keys[ROWS][COLUMNS] = {"123456789",
                             "         ",
                             "  < v >  "};
 
-char pins_in[COLUMNS]               = {PC6,    PD0,    PD1,    PD2,    PD3,    PD4,    PB6,    PB7,    PD5};
-volatile uint8_t *p_in[COLUMNS]     = {&PINC,  &PIND,  &PIND,  &PIND,  &PIND,  &PIND,  &PINB,  &PINB,  &PIND};
-volatile uint8_t *ports_in[COLUMNS] = {&PORTC, &PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTB, &PORTB, &PORTD};
+char column_pins[COLUMNS]                       = {PC6,    PD0,    PD1,    PD2,    PD3,    PD4,    PB6,    PB7,    PD5};
+volatile uint8_t *column_read_pins[COLUMNS]     = {&PINC,  &PIND,  &PIND,  &PIND,  &PIND,  &PIND,  &PINB,  &PINB,  &PIND};
+volatile uint8_t *column_ports[COLUMNS]         = {&PORTC, &PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTB, &PORTB, &PORTD};
 
-char pins_out[ROWS]               = {PD6,    PD7,    PB0};
-volatile uint8_t *ports_out[ROWS] = {&PORTD, &PORTD, &PORTB};
+char row_pins[ROWS]               = {PD6,    PD7,    PB0};
+volatile uint8_t *row_ports[ROWS] = {&PORTD, &PORTD, &PORTB};
 
 void keypad_init() {
     for(int i = 0; i < ROWS; i++) {
-        volatile uint8_t *port_out = ports_out[i];
-        char pin_out = pins_out[i];
+        volatile uint8_t *port = row_ports[i];
+        char pin = row_pins[i];
 
-        *port_out |= (1 << pin_out);
+        *port |= (1 << pin);
     }
 
     for(int i = 0; i < COLUMNS; i++) {
-        volatile uint8_t *port_in = ports_in[i];
-        char pin_in = pins_in[i];
+        volatile uint8_t *port = column_ports[i];
+        char pin = column_pins[i];
 
-        *port_in &= ~(1 << pin_in);
+        *port &= ~(1 << pin);
     }
 } 
 
@@ -39,25 +39,28 @@ void print(char *s) {
 }
 
 char keypad_poll() {
-    for(int out = 0; out < ROWS; out++) {
-        volatile uint8_t *port_out = ports_out[out];
-        char pin_out = pins_out[out];
+    for(int row = 0; row < 1; row++) {
+        volatile uint8_t *row_port = row_ports[row];
+        char row_pin = row_pins[row];
 
-        *port_out |= (1 << pin_out);
+        *row_port |= (1 << row_pin);
         _delay_ms(20);
 
-        for(int in = 0; in < COLUMNS; in++) {
-            volatile uint8_t *port_in = p_in[in];
-            char pin_in = pins_in[in];
+        for(int col = 0; col < 1; col++) {
+            volatile uint8_t *col_read_pin = column_read_pins[col];
+            char col_pin = column_pins[col];
 
-            if((*port_in) & (1 << pin_in)) {  
-                *port_out &= ~(1 << pin_out);
+            
+            if((*col_read_pin) & (1 << col_pin)) {  
+                *row_port &= ~(1 << row_pin);
+
                 print("hello!\n");
-                return keys[out][in];
+
+                return keys[row][col];
             }
         }
 
-        *port_out &= ~(1 << pin_out);
+        *row_port &= ~(1 << row_pin);
     }
 
     return 0;
