6c7da2f753f1d0a66de812e887a13b0d9f859e45
Arthur Luz <arthur@nubo.ai>
2021-11-15 14:42:23 -0300

    deletando pasta Testes

--- Testes/adc.h
+++
@@ -1,3 +0,0 @@
-void adc_init();
-void adc_set_channel(uint8_t ch);
-uint16_t adc_read();
--- Testes/print.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PRINT_H
-#define _PRINT_H
-
-void printint(uint32_t n);
-void printhex(uint32_t n);
-void printfloat(float n);
-void print(char *s);
-
-#endif
